828e2764fc94e93c79e13d5b2c1df2947dc83eea
khloptsev <p.khloptsev@gmail.com>
2022-07-29 17:17:40 +1000

    add select component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './styles/App.css';
 import React, { useState } from 'react';
 import PostsList from './components/PostsList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 const App = () => {
   const [posts, setPosts] = useState([
@@ -9,6 +11,8 @@ const App = () => {
     { id: 2, title: 'Java', body: 'Java - язык программирования' },
     { id: 3, title: 'Python', body: 'Python - язык программирования' },
   ])
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,9 +22,31 @@ const App = () => {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  }
+
   return (
     <div className='App'>
       <PostForm createPost={createPost} />
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder='Поиск...'
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            { value: 'title', name: 'по названию' },
+            { value: 'body', name: 'по описанию' },
+          ]}
+        />
+      </div>
       {posts.length 
         ? 
         <PostsList remove={removePost} posts={posts} title={'Список постов'} />
